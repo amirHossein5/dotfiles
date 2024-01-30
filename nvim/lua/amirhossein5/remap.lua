461af6da44c4f86128d79d84c449cb35dd695326
amirHossein5 <amir5665@yahoo.com>
2024-01-30 22:44:53 +0330

    Edit duplicated nvim remap

--- nvim/lua/amirhossein5/remap.lua
+++ nvim/lua/amirhossein5/remap.lua
@@ -14,7 +14,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("x", "<leader>p", [["_dp]])
+vim.keymap.set("x", "<leader>P", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
