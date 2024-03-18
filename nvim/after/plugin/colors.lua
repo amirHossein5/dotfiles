5661f9ffca9209527be4e91f91b62396559092ba
amirHossein5 <amir5665@yahoo.com>
2024-03-18 18:23:27 +0330

    Add diff colors for rose-pine theme

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -9,6 +9,11 @@ function SetTheme(theme)
 		vim.cmd("hi StatusLine guibg=#1f1d2e")
 		vim.cmd("hi ColorColumn guibg=#1f1d2e")
 	end
+
+	if theme == 'rose-pine' then
+		vim.cmd("hi diffAdd guifg=#67a95b guibg=NONE")
+		vim.cmd("hi diffDelete guifg=#FA5057 guibg=NONE")
+	end
 end
 
 SetTheme('rose-pine')
