e1bd28d0cc37c746e230e157cedd740a3349e5ac
amirHossein5 <amir5665@yahoo.com>
2024-03-18 17:03:59 +0330

    Add treesitter for html

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "vimdoc", "javascript", "typescript", "c", "lua", "rust", "php" },
+    ensure_installed = { "vimdoc", "html", "javascript", "typescript", "c", "lua", "rust", "php" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
@@ -8,8 +8,6 @@ require 'nvim-treesitter.configs'.setup {
     -- Automatically install missing parsers when entering buffer
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = true,
-
-    ignore_install = { "html" },
 
     indent = {
         enable = true,
