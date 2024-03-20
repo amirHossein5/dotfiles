8986c006bad3e9154cad6c5ffa76d25bc600f653
amirHossein5 <amir5665@yahoo.com>
2024-03-20 20:08:02 +0330

    Add autocmd to remove end whitepaces

--- nvim/lua/amirhossein5/init.lua
+++ nvim/lua/amirhossein5/init.lua
@@ -3,3 +3,8 @@ vim.cmd('source ~/.config/nvim/lua/amirhossein5/set.vim')
 require('amirhossein5.remap')
 
 vim.cmd('source ~/.config/nvim/lua/amirhossein5/blade.vim')
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+    group = vim.api.nvim_create_augroup("Personal", {}),
+    pattern = "*",
+    command = [[:exec 'norm m`' | %s/\s\+$//e | norm g``]]
+})
