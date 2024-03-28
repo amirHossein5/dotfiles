30061ea4e39eda7389ca25556216bafc9cbcce07
amirHossein5 <amir5665@yahoo.com>
2024-03-28 22:37:59 +0330

    Add package lock and composer lock to vim rooter patterns

--- nvim/lua/amirhossein5/lazy/vim-rooter.lua
+++ nvim/lua/amirhossein5/lazy/vim-rooter.lua
@@ -3,7 +3,7 @@ return {
 
     config = function()
         vim.g.rooter_manual_only = 1
-        vim.g.rooter_patterns = { '.git', 'composer.json', 'package.json', '.gitignore' }
+        vim.g.rooter_patterns = { '.git', 'composer.json', 'composer.lock', 'package.json', 'package-lock.json', '.gitignore' }
         vim.cmd('Rooter')
     end
 }
