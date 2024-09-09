2aaed94bd5361ee1b1319c455fa63c450d8f75d1
amirHossein5 <amir5665@yahoo.com>
2024-09-09 08:49:53 +0330

    fix: remove treesitter html highlighting

--- nvim/lua/amirhossein5/lazy/treesitter.lua
+++ nvim/lua/amirhossein5/lazy/treesitter.lua
@@ -15,6 +15,10 @@ return {
                 enable = true,
 
                 disable = function(lang, buf)
+                    if lang == 'html' then
+                        return true
+                    end
+
                     local max_filesize = 200 * 1024 -- 200 KB
                     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
                     if ok and stats and stats.size > max_filesize then
