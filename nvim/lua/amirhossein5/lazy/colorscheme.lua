9049bcfffb1e45886b2a09ec389b306d79038317
amirHossein5 <amir5665@yahoo.com>
2024-11-09 22:55:20 +0330

    fix: make colurColumn dimmer

--- nvim/lua/amirhossein5/lazy/colorscheme.lua
+++ nvim/lua/amirhossein5/lazy/colorscheme.lua
@@ -3,6 +3,8 @@ function SetTheme(theme)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+    vim.cmd("highlight ColorColumn guibg=#0d0c0c")
 end
 
 return {
