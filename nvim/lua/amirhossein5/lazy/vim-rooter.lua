8754498ed890324757416c4c8343e8ff84628883
amirHossein5 <amir5665@yahoo.com>
2025-01-01 21:22:34 +0330

    feat: delete vim-rooter and auto-pairs plugins

--- nvim/lua/amirhossein5/lazy/vim-rooter.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'airblade/vim-rooter',
-
-    config = function()
-        vim.g.rooter_manual_only = 1
-        vim.g.rooter_patterns = { '.git', 'composer.json', 'composer.lock', 'package.json', 'package-lock.json', '.gitignore' }
-        vim.cmd('Rooter')
-    end
-}
